efbcba6a1b598bd170167273c8d81363aa6a372b
Madhuni96 <43632067+Madhuni96@users.noreply.github.com>
2020-03-25 15:35:58 +0530

    Update some types in song_mod.js

--- API/MODELS/song_mod.js
+++ API/MODELS/song_mod.js
@@ -1,63 +1,63 @@
-const mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
 
 const songSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
 
-    sinhalaTitle: {
-        type: String,
-        required: [true, 'Required'],
-        unique: true
+  sinhalaTitle: {
+    type: String,
+    required: [true, "Required"],
+    unique: true
+  },
+
+  singlishTitle: {
+    type: String,
+    required: [true, "Required"],
+    unique: true
+  },
+
+  artistName: {
+    type: Array,
+    required: [true, "Required"]
+  },
+
+  artistId: {
+    type: Array,
+    required: [true, "Required"]
+  },
+
+  artist: {
+    method: {
+      type: String,
+      required: [true, "Required"]
     },
-
-    singlishTitle: {
-        type: String,
-        required: [true, 'Required'],
-        unique: true
-    },
-
-    artistName: {
-        type: String,
-        required: [true, 'Required'],
-    },
-
-    artistId: {
-        type: String,
-        required: [true, 'Required'],
-    },
-
-    artist: {
-        method: {
-            type: String,
-            required: [true, 'Required'],
-        },
-        url: {
-            type: String,
-            required: [true, 'Required'],
-        }
-    },
-
-    categories: {
-        type: Array,
-        required: [true, 'Required'],
-    },
-
-    song: {
-        type: String,
-        required: [true, 'Required'],
-    },
-
-    likes: {
-        type: Number,
-        default: 0
-    },
-
-    type: {
-        type: String,
-        required: [true, 'Required'],
+    url: {
+      type: String,
+      required: [true, "Required"]
     }
+  },
+
+  categories: {
+    type: Array,
+    required: [true, "Required"]
+  },
+
+  song: {
+    type: String,
+    required: [true, "Required"]
+  },
+
+  likes: {
+    type: Number,
+    default: 0
+  },
+
+  type: {
+    type: String,
+    required: [true, "Required"]
+  }
 });
 
-songSchema.plugin(uniqueValidator, { message: '{VALUE} is already exists' });
+songSchema.plugin(uniqueValidator, { message: "{VALUE} is already exists" });
 
-module.exports = mongoose.model('Song', songSchema);
+module.exports = mongoose.model("Song", songSchema);
