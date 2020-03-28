1c3a4b7be7ab8dd9d271aa3eebab63bd21bbb229
Kalana-Ramesh <58284255+Kalana-Ramesh@users.noreply.github.com>
2020-03-28 21:42:38 +0530

    api check

--- app.js
+++ app.js
@@ -50,4 +50,16 @@ mongoose.connect(
 
 app.use("/", router);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the codebright-lyrics-server");
+});
+
+const PORT = process.env.PORT || 8080;
+var server = app.listen(PORT, () => {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("***Server listening at http://%s:%s***", host, port);
+});
+
 module.exports = app;
